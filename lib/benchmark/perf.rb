1e947941741ff78354073e93186d7bb007a9b8fb
Piotr Murach <pmurach@gmail.com>
2018-09-16 12:28:57 +0200

    Change to be consistent

--- lib/benchmark/perf.rb
+++ lib/benchmark/perf.rb
@@ -16,10 +16,11 @@ module Benchmark
     #   the average of given measurements
     #
     # @api public
-    def self.average(measurements)
+    def average(measurements)
       return 0 if measurements.empty?
       measurements.reduce(&:+).to_f / measurements.size
     end
+    module_function :average
 
     # Calculate variance of measurements
     #
@@ -28,7 +29,7 @@ module Benchmark
     # @return [Float]
     #
     # @api public
-    def self.variance(measurements)
+    def variance(measurements)
       return 0 if measurements.empty?
       avg = average(measurements)
       total = measurements.reduce(0) do |sum, x|
@@ -36,16 +37,18 @@ module Benchmark
       end
       total.to_f / measurements.size
     end
+    module_function :variance
 
     # Calculate standard deviation
     #
     # @param [Array[Float]] measurements
     #
     # @api public
-    def self.std_dev(measurements)
+    def std_dev(measurements)
       return 0 if measurements.empty?
       Math.sqrt(variance(measurements))
     end
+    module_function :std_dev
 
     # Run given work and gather time statistics
     #
@@ -54,10 +57,11 @@ module Benchmark
     # @return [Boolean]
     #
     # @api public
-    def self.assert_perform_under(threshold, options = {}, &work)
+    def assert_perform_under(threshold, options = {}, &work)
       actual, _ = ExecutionTime.run(options, &work)
       actual <= threshold
     end
+    module_function :assert_perform_under
 
     # Assert work is performed within expected iterations per second
     #
@@ -66,31 +70,35 @@ module Benchmark
     # @return [Boolean]
     #
     # @api public
-    def self.assert_perform_ips(iterations, options = {}, &work)
+    def assert_perform_ips(iterations, options = {}, &work)
       mean, stddev, _ = Iteration.run(options, &work)
       iterations <= (mean + 3 * stddev)
     end
+    module_function :assert_perform_ips
 
     if defined?(Process::CLOCK_MONOTONIC)
       # Object representing current time
-      def self.time_now
+      def time_now
         Process.clock_gettime Process::CLOCK_MONOTONIC
       end
+      module_function :time_now
     else
       # Object represeting current time
-      def self.time_now
+      def time_now
         Time.now
       end
+      module_function :time_now
     end
 
     # Measure time elapsed with a monotonic clock
     #
     # @public
-    def self.clock_time
+    def clock_time
       before = time_now
       yield
       after = time_now
       after - before
     end
+    module_function :clock_time
   end # Perf
 end # Benchmark
