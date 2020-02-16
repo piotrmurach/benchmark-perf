c8344c739d3b3bc97df9601791242e55e4eee4c8
Piotr Murach <pmurach@gmail.com>
2020-02-16 13:57:01 +0000

    Add stats module

--- spec/unit/stats_spec.rb
+++ spec/unit/stats_spec.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
-RSpec.describe Benchmark::Perf, 'arithmetic' do
+RSpec.describe Benchmark::Perf::Stats do
   context '#average' do
     it "calculates average without measurements" do
-      expect(Benchmark::Perf.average([])).to eq(0)
+      expect(described_class.average([])).to eq(0)
     end
 
     it "calculates average with measurements" do
-      expect(Benchmark::Perf.average([1,2,3])).to eq(2.0)
+      expect(described_class.average([1,2,3])).to eq(2.0)
     end
   end
 
   context '#variance' do
     it "calculates variance of no measurements" do
-      expect(Benchmark::Perf.variance([])).to eq(0)
+      expect(described_class.variance([])).to eq(0)
     end
 
     it "calculates variance of measurements" do
-      expect(Benchmark::Perf.variance([1,2,3])).to eq(2.to_f/3)
+      expect(described_class.variance([1,2,3])).to eq(2.to_f/3)
     end
   end
 
   context '#std_dev' do
     it "calculates standard deviation of no measurements" do
-      expect(Benchmark::Perf.std_dev([])).to eq(0)
+      expect(described_class.std_dev([])).to eq(0)
     end
 
     it "calculates standard deviation of measurements" do
-      expect(Benchmark::Perf.std_dev([1,2,3])).to eq(Math.sqrt(2.to_f/3))
+      expect(described_class.std_dev([1,2,3])).to eq(Math.sqrt(2.to_f/3))
     end
   end
 end
